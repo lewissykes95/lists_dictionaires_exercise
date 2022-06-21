f6ddbcfcd655faf705335c7c0e325fa29f4c6511
Lewis <lewisdsykes@gmail.com>
2022-06-21 16:26:16 +0100

    finished a5

--- exercise_a_stops.py
+++ exercise_a_stops.py
@@ -9,8 +9,9 @@ stops.insert(4, "Polmont")
 #4. Print out the index position of "Linlithgow"
 print(stops.index("Linlithgow"))
 #5. Remove "Livingston" from the list using its name
-
+stops.remove("Livingston")
 #6. Delete "Cumbernauld" from the list by index
+
 #7. Print the number of stops there are in the list
 #8. Sort the list alphabetically
 #9. Reverse the positions of the stops in the list

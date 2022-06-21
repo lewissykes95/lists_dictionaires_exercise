7129032b0505ffb02c6dedb49c4e2a1ea7403903
Lewis <lewisdsykes@gmail.com>
2022-06-21 20:25:03 +0100

    finished b8

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -77,7 +77,9 @@ for number in avril_lottery_numbers:
 users["Erik"]["lottery_numbers"].append(7)
 print(users["Erik"]["lottery_numbers"])
 # 8. Change Erik's hometown to Edinburgh
-
+users["Erik"]["home_town"] = "Edinburgh"
 
 # 9. Add a pet dog to Erik called "fluffy"
+
+
 # 10. Add another person to the users dictionary

620c2e4ba7b0a301fddcf8421f5f58fe3e35508a
Lewis <lewisdsykes@gmail.com>
2022-06-21 16:22:24 +0100

    finished number a3

--- exercise_a_stops.py
+++ exercise_a_stops.py
@@ -3,9 +3,11 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 #1. Add "Edinburgh Waverley" to the end of the list
 stops.append("Edinburgh Waverley")
 #2. Add "Glasgow Queen St" to the start of the list
-stops[0] = "Glasgow Queen St"
+stops.insert(0, "Glasgow Queen St")
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+stops.insert(4, "Polmont")
 #4. Print out the index position of "Linlithgow"
+
 #5. Remove "Livingston" from the list using its name
 #6. Delete "Cumbernauld" from the list by index
 #7. Print the number of stops there are in the list
@@ -13,4 +15,4 @@ stops[0] = "Glasgow Queen St"
 #9. Reverse the positions of the stops in the list
 #10 Print out all the stops using a for loop
 
-
+print(stops)
